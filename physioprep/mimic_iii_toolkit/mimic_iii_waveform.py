5a1d5adaf3d4e225726ba8fc6acd6dd970b290da
unknown <saadatmilad1792@gmail.com>
2025-09-03 22:24:58 -0500

    QuickDrawTest

--- physioprep/mimic_iii_toolkit/mimic_iii_waveform.py
+++ physioprep/mimic_iii_toolkit/mimic_iii_waveform.py
@@ -6,6 +6,7 @@ import gc
 import wfdb
 import pooch
 import requests
+import pandas as pd
 from tqdm import tqdm
 
 ########################################################################################################################
@@ -20,7 +21,6 @@ class M3WaveFormMasterClass():
       "physionet_url": "https://physionet.org/files/",
       "physionet_dir": "mimic3wdb-matched/1.0/",
     }
-    self.available_signals = None
     self.update_new_arguments(args)
 
   def update_new_arguments(self, args: dict) -> None:
@@ -57,9 +57,9 @@ class M3WaveFormMasterClass():
 
     return layout_file
 
-  def get_all_available_signals(self):
+  def get_all_available_signals(self) -> pd.DataFrame:
+    wave_form_type_df = []
     patients = self.get_patients()
-    available_signals = []
     pbar = tqdm(patients, desc = "Loading patients")
     for i, patient_group_id in enumerate(pbar):
       pbar.set_description(f"{i+1} out of {len(patients)}")
@@ -67,23 +67,39 @@ class M3WaveFormMasterClass():
       pn_dir = self.args["physionet_dir"] + group + "/" + pid
       records = self.get_record_list(self.args["physionet_url"] + pn_dir + "/" + "RECORDS")
       for record in records:
-        available_signals.extend(self.get_available_signals(pn_dir, record))
-        available_signals = list(set(available_signals))
+        wave_form_type_df.append(pd.DataFrame({
+          "patient_group": group,
+          "patient_id": pid,
+          "patient_record": record,
+          "patient_signals": [self.get_available_signals(pn_dir, record)],
+        }))
+        
+    return pd.concat(wave_form_type_df)
 
-    self.available_signals = available_signals
-    return available_signals
-
-  def load_patient_record(self, patient_group_id):
+  def load_patient_record(self, patient_group_id: str, patient_filter: list[str] = None) -> list[object]:
     group, pid = self.get_patient_group_id(patient_group_id)
     pn_dir = self.args["physionet_dir"] + group + "/" + pid
     records = self.get_record_list(self.args["physionet_url"] + pn_dir + "/" + "RECORDS")
 
     records_list = []
     for record in records:
+      if patient_filter is not None:
+        signals = self.get_available_signals(pn_dir, record)
+        if not set(patient_filter).issubset(signals):
+          continue
+
+      print(patient_group_id, record)
       rec = wfdb.rdrecord(record, pn_dir = pn_dir)
       records_list.append({"group": group, "pid": pid, "record": record, "record_data": rec})
       del rec
       gc.collect()
-      break
 
     return records_list
+  
+  def load_all_patient_record(self, patient_filter: list[str] = None) -> list[object]:
+    patient_record = []
+    patients = self.get_patients()
+    for patient_group_id in patients:
+      patient_record.extend(self.load_patient_record(patient_group_id, patient_filter))
+
+    return patient_record
