739698182b80e8536e2132f3f50a38b963c6afdc
unknown <saadatmilad1792@gmail.com>
2025-09-07 15:07:00 -0500

    Get Batch Added; Tests Added;

--- src/physioprep/mimic_iii_toolkit/data_handler.py
+++ src/physioprep/mimic_iii_toolkit/data_handler.py
@@ -5,8 +5,27 @@ import re
 import wfdb
 import pooch
 import requests
+import numpy as np
 import pandas as pd
 from importlib import resources
+
+########################################################################################################################
+## -- mimic iii waveform database data handler module -- ###############################################################
+########################################################################################################################
+class M3ValidationMasterClass():
+  def __init__(self):
+    super(M3ValidationMasterClass, self).__init__()
+
+  def is_item_not_nan(self, batch_item: np.array) -> bool:
+    return not np.isnan(batch_item).any()
+  
+  def is_length_valid(self, batch_item: np.array, sig_len: int) -> bool:
+    return batch_item.shape[0] == sig_len
+
+  def apply(self, batch_item: np.array, sig_len: int) -> bool:
+    valid_flag = self.is_item_not_nan(batch_item = batch_item) and \
+                 self.is_length_valid(batch_item = batch_item, sig_len = sig_len)
+    return valid_flag
 
 ########################################################################################################################
 ## -- mimic iii waveform database data handler module -- ###############################################################
@@ -17,6 +36,7 @@ class M3WaveFormMasterClass():
     with resources.open_binary("physioprep.data", "patient_signals.pkl") as file:
       patients_list_csv = pd.read_pickle(file)
 
+    self.validation = M3ValidationMasterClass()
     self.args_preset = {
       "dat_cache_dir": pooch.os_cache('wfdb'),
       "physionet_url": "https://physionet.org/files/",
@@ -50,7 +70,7 @@ class M3WaveFormMasterClass():
 
   ## -- get patients that have the listed signals available -- ##
   def get_patient_with_signal(self, patients: list[str] | None = None, 
-                               signal_filter: list | None = None) -> pd.DataFrame:
+                              signal_filter: list[str] | None = None) -> pd.DataFrame:
     
     df = self.args_preset["patients_list"].copy()
     patients = patients if patients is not None else list(df["patient_id"])
@@ -62,6 +82,54 @@ class M3WaveFormMasterClass():
   ## -- get patient record as a dataset -- ##
   def get_patient_record(self, group: str, pid: str, record: str, sampfrom: int | None  = 0, 
                          sampto: int | None  = None, channels: list[int] | None = None) -> wfdb.Record:
+
+    df = self.args_preset["patients_list"].copy()
+    available_channels = df[(df["patient_id"] == pid) & (df["patient_record"] == record)].iloc[0]
+    channels = channels if channels is not None else available_channels["patient_signals"]
+    channels = [available_channels["patient_signals"].index(item) for item in channels]
     pn_dir = self.args_preset["physionet_dir"] + group + "/" + pid
     rec = wfdb.rdrecord(record, pn_dir = pn_dir, sampfrom = sampfrom, sampto = sampto, channels = channels)
     return rec
+  
+  ## -- get patient record as a header -- ##
+  def get_patient_header(self, group: str, pid: str, record: str) -> wfdb.Record:
+    pn_dir = self.args_preset["physionet_dir"] + group + "/" + pid
+    header = wfdb.rdheader(record, pn_dir = pn_dir)
+    return header
+
+  ## -- splits the available subjects into three separate dataframes for train, test, and validation -- ##
+  def get_subject_split(self, df: pd.DataFrame, frac1: float = 0.8, frac2: float = 0.1, frac3: float = 0.1, 
+                        seed: int | None = None, channels: list[str] | None = None) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+
+    total = frac1 + frac2 + frac3
+    frac1, frac2, frac3 = frac1/total, frac2/total, frac3/total
+    df = self.get_patient_with_signal(patients = df["patient_id"], signal_filter = channels)
+    df_shuffled = df.sample(frac = 1, random_state = seed).reset_index(drop = True)
+    
+    s1 = int(len(df) * frac1)
+    s2 = int(len(df) * frac2)
+    
+    df1 = df_shuffled.iloc[:s1]
+    df2 = df_shuffled.iloc[s1:s1+s2]
+    df3 = df_shuffled.iloc[s1+s2:]
+    
+    return df1, df2, df3
+
+  ## -- selects a random batch from the data -- ##
+  def get_data_batch(self, df: pd.DataFrame, batch_size: int, signal_len: int, 
+                     channels: list[str] | None = None, timeout: int = 100) -> np.array:
+
+    batch, timeout_counter = [], 0
+    while len(batch) < batch_size and timeout_counter <= timeout:
+      timeout_counter += 1
+      rand_row = df.sample(n = 1).iloc[0]
+      group, pid, record = rand_row["patient_group"], rand_row["patient_id"], rand_row["patient_record"]
+      header = self.get_patient_header(group, pid, record)
+      random_offset = np.random.randint(0, header.sig_len - signal_len)
+      sampfrom, sampto = random_offset, random_offset + signal_len
+      rec = self.get_patient_record(group, pid, record, sampfrom = sampfrom, sampto = sampto, channels = channels)
+      waveform, val_flag = rec.p_signal, self.validation.apply(rec.p_signal, signal_len)
+      batch.append(waveform.transpose(1, 0)) if val_flag else None
+      # print(rec.p_signal.shape, header.fs, rec.sig_name)
+
+    return np.stack(batch)
--- tests/mimic_iii_toolkit/test_data_handler.py
+++ tests/mimic_iii_toolkit/test_data_handler.py
@@ -1,8 +1,52 @@
 ########################################################################################################################
 ## -- libraries and packages -- ########################################################################################
 ########################################################################################################################
+import numpy as np
 import physioprep
 
+########################################################################################################################
+## -- tests for validation master class -- #############################################################################
+########################################################################################################################
+def val():
+  return physioprep.M3ValidationMasterClass()
+
+def test_is_item_not_nan():
+  nan_arr = np.array([[1, np.nan, 2], [1, 4, 2]])
+  flag = val().is_item_not_nan(nan_arr)
+  assert not flag
+
+  nan_arr = np.array([[1, 0, 2], [1, 4, 2]])
+  flag = val().is_item_not_nan(nan_arr)
+  assert flag
+
+def test_is_length_valid():
+  len_arr = np.array([1, 2, 3, 4, 5])
+  flag = val().is_length_valid(len_arr, 5)
+  assert flag
+
+  len_arr = np.array([1, 2, 3, 4, 5, 3, 3, 1])
+  flag = val().is_length_valid(len_arr, 5)
+  assert not flag
+  
+def test_apply():
+  nan_arr = np.array([[1, np.nan, 2], [1, 4, 2]])
+  flag = val().apply(nan_arr, 2)
+  assert not flag
+
+  flag = val().apply(nan_arr, 3)
+  assert not flag
+
+  nan_arr = np.array([[1, 0, 2, 5], [1, 4, 2, 4]])
+  flag = val().apply(nan_arr, 3)
+  assert not flag
+
+  nan_arr = np.array([[1, 0, 2], [1, 4, 2], [2, 8, 2]])
+  flag = val().apply(nan_arr, 3)
+  assert flag
+
+########################################################################################################################
+## -- tests for physioprep module [mimic iii toolkit] -- ###############################################################
+########################################################################################################################
 def ppm():
   return physioprep.M3WaveFormMasterClass()
 
@@ -39,3 +83,30 @@ def test_get_patient_with_signal():
 
   df = ppm().get_patient_with_signal(patients = ["p000020"], signal_filter = ['ABP'])
   assert len(df) == 1
+
+def test_get_patient_record():
+  df = ppm().args_preset["patients_list"].iloc[0]
+  rec = ppm().get_patient_record(df["patient_group"], df["patient_id"], df["patient_record"], 
+                                 sampfrom = 20000, sampto = 20500, channels = ['II', 'ABP'])
+  
+  sig_mismatch = set(rec.sig_name) - set(['II', 'ABP'])
+  assert not sig_mismatch
+  assert rec.p_signal.shape == (500, 2)
+
+def test_get_patient_header():
+  df = ppm().args_preset["patients_list"].iloc[0]
+  header = ppm().get_patient_header(df["patient_group"], df["patient_id"], df["patient_record"])
+  assert header.sig_len == 9862593
+
+def test_get_subject_split():
+  df = ppm().args_preset["patients_list"]
+  a, b, c = ppm().get_subject_split(df, channels = ['ABP', 'II', 'PLETH'])
+  assert len(a) == 4784 and len(b) == 598 and len(c) == 598
+
+def test_():
+  channels = ['ABP', 'II', 'PLETH']
+  df = ppm().args_preset["patients_list"]
+  tr_df, va_df, te_df = ppm().get_subject_split(df, channels = channels)
+  batch = ppm().get_data_batch(tr_df, batch_size = 12, signal_len = 750, channels = channels)
+  assert batch.shape[1:] == (3, 750)
+  assert batch.shape[0] <= 12
