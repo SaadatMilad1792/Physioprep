7eee3bc42f657c52f98768d7a14bfd5b6fe289af
unknown <saadatmilad1792@gmail.com>
2025-09-06 19:14:45 -0500

    Data Handler Added;

--- src/physioprep/mimic_iii_toolkit/data_handler.py
+++ src/physioprep/mimic_iii_toolkit/data_handler.py
@@ -1,6 +1,7 @@
 ########################################################################################################################
 ## -- libraries and packages -- ########################################################################################
 ########################################################################################################################
+import re
 import wfdb
 import pooch
 import requests
@@ -13,8 +14,8 @@ from importlib import resources
 class M3WaveFormMasterClass():
   def __init__(self) -> None:
     super(M3WaveFormMasterClass, self).__init__()
-    with resources.open_text("physioprep.data", "patient_signals.csv") as file:
-      patients_list_csv = pd.read_csv(file)
+    with resources.open_binary("physioprep.data", "patient_signals.pkl") as file:
+      patients_list_csv = pd.read_pickle(file)
 
     self.args_preset = {
       "dat_cache_dir": pooch.os_cache('wfdb'),
@@ -24,7 +25,7 @@ class M3WaveFormMasterClass():
     }
 
   ## -- get the list of patients from preset .csv or from physionet -- ##
-  def get_patients(self, load_preset = True):
+  def get_patients(self, load_preset: bool = True) -> list[str]:
     if load_preset:
       patients_list = self.args_preset["patients_list"]
       patients_list = patients_list.apply(lambda r: f"{r['patient_group']}/{r['patient_id']}/", axis = 1).tolist()
@@ -35,3 +36,32 @@ class M3WaveFormMasterClass():
       patients_list = requests.get(patients_url).text.strip().split("\n")
 
     return list(patients_list)
+  
+  ## -- get the group and id for a single patient entry of form "pXX/pXXXXXX/" -- ##
+  def get_patient_group_id(self, patient_group_id: str) -> tuple[str, str]:
+    group, pid = re.match("([^/]+)/([^/]+)/", patient_group_id).groups()
+    return group, pid
+
+  ## -- get all the available signals -- ##
+  def get_available_signals(self) -> list[str]:
+    forbidden = ['???']
+    unique_signals = self.args_preset["patients_list"]["patient_signals"].explode().dropna().unique()
+    return [s for s in unique_signals if s not in forbidden]
+
+  ## -- get patients that have the listed signals available -- ##
+  def get_patient_with_signal(self, patients: list[str] | None = None, 
+                               signal_filter: list | None = None) -> pd.DataFrame:
+    
+    df = self.args_preset["patients_list"].copy()
+    patients = patients if patients is not None else list(df["patient_id"])
+    df = df[df["patient_id"].isin(patients)]
+    if signal_filter is not None:
+      df = df[df["patient_signals"].apply(lambda sig: set(signal_filter).issubset(sig))]
+    return df
+
+  ## -- get patient record as a dataset -- ##
+  def get_patient_record(self, group: str, pid: str, record: str, sampfrom: int | None  = 0, 
+                         sampto: int | None  = None, channels: list[int] | None = None) -> wfdb.Record:
+    pn_dir = self.args_preset["physionet_dir"] + group + "/" + pid
+    rec = wfdb.rdrecord(record, pn_dir = pn_dir, sampfrom = sampfrom, sampto = sampto, channels = channels)
+    return rec
--- tests/mimic_iii_toolkit/test_data_handler.py
+++ tests/mimic_iii_toolkit/test_data_handler.py
@@ -1,3 +1,41 @@
 ########################################################################################################################
 ## -- libraries and packages -- ########################################################################################
 ########################################################################################################################
+import physioprep
+
+def ppm():
+  return physioprep.M3WaveFormMasterClass()
+
+def test_get_patients() -> None:
+  patients_preset = ppm().get_patients(load_preset = True)
+  patients_reqset = ppm().get_patients(load_preset = False)
+  only_in_preset = set(patients_preset) - set(patients_reqset)
+  assert not only_in_preset
+
+def test_get_patient_group_id() -> None:
+  group, pid = ppm().get_patient_group_id("p00/p000020/")
+  assert group == "p00"
+  assert pid == "p000020"
+
+def test_get_available_signals():
+  real_signals = ['L', 'II+', 'aVL', 'CO2', 'II', 'BAP', 'R', 'PLETH', 'PLETHl', 'UVP', 'RAP', 'I', 
+                  'aVR', 'MCL1+', 'AVR', 'UAP', 'IC1', 'AVF+', 'ECG', 'ART', 'III+', 'V+', 'III', 'AVL', 
+                  'I+', 'FAP', 'Ao', 'V3', 'V', 'V2', 'PAP', 'AOBP', 'V5', 'P4', 'IC2', 'ABP', 'P1', 
+                  'CVP', 'MCL+', 'LAP', 'AVR+', 'AVF', 'aVF', 'PLETHr', 'MCL', 'RESP', 'ICP', 'V1', 'MCL1']
+
+  available_signals = ppm().get_available_signals()
+  mismatch = set(available_signals) - set(real_signals)
+  assert not mismatch
+
+def test_get_patient_with_signal():
+  df = ppm().get_patient_with_signal(patients = None, signal_filter = None)
+  assert len(df) == 22494
+
+  df = ppm().get_patient_with_signal(patients = ["p000020"], signal_filter = None)
+  assert len(df) == 1
+
+  df = ppm().get_patient_with_signal(patients = None, signal_filter = ['ABP'])
+  assert len(df) == 9643
+
+  df = ppm().get_patient_with_signal(patients = ["p000020"], signal_filter = ['ABP'])
+  assert len(df) == 1
